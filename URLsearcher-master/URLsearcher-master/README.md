1f627a185d6c5e88e2dc27e720a596c720774a1f
Niklas Lundström <te14.niklas.lundstrom@umea.nti.se>
2017-06-02 08:54:58 +0200

    abc
    abc

---
+++ URLsearcher-master/URLsearcher-master/README.md
@@ -0,0 +1,2 @@
+# URLsearcher
+Niklas och sErgii
---
+++ URLsearcher-master/URLsearcher-master/src/controller.java
@@ -0,0 +1,8 @@
+/**
+ * Created by shse14 on 2017-01-27.
+ */
+public class controller {
+    private view theView;
+    private model theModel;
+
+    this.theView.addGoListener(new GoButtonListener());
